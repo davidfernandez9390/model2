15718cd7f4b81e17c073b814de75c90371903fb2
davidfernandez9390 <david.fernanlpz@gmail.com>
2025-05-12 22:46:50 +0200

    Update shiny_interface.R
    shiny_interface.R per fer-lo estratificat

--- shiny_interface.R
+++ shiny_interface.R
@@ -3,47 +3,62 @@ library(ggplot2)
 source('model.R')
 
 get.strategies <- function() {
+  #retorna un vector de strings amb els noms de les estratègies
   return(c('untreated', 'screening', 'treatment'))
 }
 
 get.parameters <- function() {
+  #retorna una llista de llistes on cada subllista té els elements:
+  #name = string amb el nom (obligatori)
+  #base.value= valor numèric base (obligatori)
+  #max.value=ELIMINAR-LO
+  #stratum= string amb el nom de l'estrat si el paràmetre està estratificat (opcional)
+  #class= string amb el tipus de paràmetre per possible classificación en l'input de l'interfície (opcional)
   return(list(
     list(
       name='p.healthy.cancer',
-      base.value=0.05,
+      base.value=0.9,
       max.value=1,
-      stratum="30-35"
+      stratum="30-35", 
+      class="probability"
     ),
     list(
       name='p.healthy.cancer',
-      base.value=0.09,
+      base.value=1,
       max.value=1,
-      stratum="35-40"
+      stratum="35-40",
+      class="probability"
     ),
     list(
       name='p.healthy.death',
-      base.value=0.005
+      base.value=0.005,
+      class="probability"
     ),
     list(
       name='p.cancer.death',
-      base.value=0.5
+      base.value=0.5,
+      class="probability"
     ),
     list(
       name='p.screening.effective',
-      base.value=0.5
+      base.value=0.5,
+      class="probability"
     ),
     list(
       name='p.treatment.effective',
-      base.value=0.005
+      base.value=0.005,
+      class="probability"
     ),
     list(
       name='cost.screening',
-      base.value=500
+      base.value=500,
+      class="cost"
     ),
     list(
       name='cost.cancer.treatment',
       base.value=10000,
-      max.value=50000
+      max.value=50000,
+      class="cost"
     ),
     list(
       name='utility.cancer',
@@ -60,21 +75,32 @@ get.parameters <- function() {
   ))
 }
 
-run.simulation <- function(strategies, pars) {
+run.simulation <- function(strategies, pars, progress = NULL) {
+  
+  # Exemple de l'estructura de pars:
+  #   pars = list(
+  #     'p.healthy.cancer'= list("30-35" = 0.05, "35-40" = 0.1),  # paràmetre estratificat
+  #     "cost.treatment" = 10003                                   # paràmetre simple
+  # 
+  #
+  # Per accedir als valors dels paràmetres de pars:
+  #   - Estratificats: pars[['p.healthy.cancer']][["35-40"]]
+  #   - Simples: pars[["cost.treatment"]]
+  
   results <- simulate(strategies,
-                     p.healthy.cancer=pars[['p.healthy.cancer']],
-                     p.healthy.death=pars[['p.healthy.death']],
-                     p.cancer.death=pars[['p.cancer.death']],
-                     p.screening.effective=pars[['p.screening.effective']],
-                     p.treatment.effective=pars[['p.treatment.effective']],
-                     cost.screening=pars[['cost.screening']],
-                     cost.cancer.treatment=pars[['cost.cancer.treatment']],
-                     utility.cancer=pars[['utility.cancer']],
-                     simulated.years=pars[['simulated.years']],
-                     discount=pars[['discount']])
+                      p.healthy.cancer_30_35=pars[['p.healthy.cancer']][['30-35']],
+                      p.healthy.cancer_35_40=pars[['p.healthy.cancer']][['35-40']],
+                      p.healthy.death=pars[['p.healthy.death']],
+                      p.cancer.death=pars[['p.cancer.death']],
+                      p.screening.effective=pars[['p.screening.effective']],
+                      p.treatment.effective=pars[['p.treatment.effective']],
+                      cost.screening=pars[['cost.screening']],
+                      cost.cancer.treatment=pars[['cost.cancer.treatment']],
+                      utility.cancer=pars[['utility.cancer']],
+                      simulated.years=pars[['simulated.years']],
+                      discount=pars[['discount']])
   return(results)
 }
-
 
 
 
